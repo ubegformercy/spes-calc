4c3e75b3f37bdd3953d66978291de45c72d7f9d4
ubegformercy <127954455+ubegformercy@users.noreply.github.com>
2023-03-15 09:02:40 -0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function calculateTime() {
+    const desiredPower = parseFloat(document.getElementById('desiredPower').value);
+    const desiredPowerUnit = parseInt(document.getElementById('desiredPowerUnit').value);
+    const currentPower = parseFloat(document.getElementById('currentPower').value);
+    const currentPowerUnit = parseInt(document.getElementById('currentPowerUnit').value);
+    const tickRate = parseFloat(document.getElementById('tickRate').value);
+    const tickRateUnit = parseInt(document.getElementById('tickRateUnit').value);
+  
+    const timeInSeconds = ((desiredPower * Math.pow(1000, desiredPowerUnit)) - (currentPower * Math.pow(1000, currentPowerUnit))) / (tickRate * Math.pow(1000, tickRateUnit));
+  
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = Math.ceil(timeInSeconds % 60);
+  
+    document.getElementById('result').innerHTML = `${hours}:${minutes}:${seconds}`;
+  }
+  
