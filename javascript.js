ac9f2084677bd1b754448d6f195aa9524ac211d9
ubegformercy <127954455+ubegformercy@users.noreply.github.com>
2023-03-16 22:03:03 -0400

    commit main

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function calculateTime() {
+function calculatePowerTime() {
     const desiredPower = parseFloat(document.getElementById('desiredPower').value);
     const desiredPowerUnit = parseInt(document.getElementById('desiredPowerUnit').value);
     const currentPower = parseFloat(document.getElementById('currentPower').value);
@@ -16,4 +16,20 @@ function calculateTime() {
     document.getElementById('result').innerHTML = `d:${String(days).padStart(2, '0')} h:${String(hours).padStart(2, '0')} m:${String(minutes).padStart(2, '0')} s:${String(seconds).padStart(2, '0')}`;
     
   }
-  
+
+function calculateTokenTime() {
+    var currentTokens = parseInt(document.getElementById("current_tokens").value);
+    var desiredTokens = parseInt(document.getElementById("desired_tokens").value);
+    var evolution = parseInt(document.getElementById("evolution").value);
+    var timeInSeconds = Math.floor((desiredTokens - currentTokens) / evolution * 60);
+
+    var days = Math.floor(timeInSeconds / (24 * 60 * 60));
+    var hours = Math.floor((timeInSeconds % (24 * 60 * 60)) / (60 * 60));
+    var minutes = Math.floor((timeInSeconds % (60 * 60)) / 60);
+
+    document.getElementById("results").innerHTML = "d:" + padZero(days) + " h:" + padZero(hours) + " m:" + padZero(minutes);
+}
+
+function padZero(num) {
+    return (num < 10) ? "0" + num : num;
+}
